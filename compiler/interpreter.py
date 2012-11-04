07a4319535d3ba71a6918931b23d4634b45d04ab
fotcorn <fotcorn@gmail.com>
2012-11-04 16:29:50 +0100

    working while implementation

--- compiler/interpreter.py
+++ compiler/interpreter.py
@@ -1,6 +1,7 @@
 from compiler.ast import VarDefinition, Identifier, Assignment, Input, Print,\
-    Constant, Expression
+    Constant, Expression, WhileStart, WhileEnd
 
+class InterpreterError(Exception): pass
 
 class Interpreter():
     
@@ -8,7 +9,14 @@ class Interpreter():
     def interpret(self, ast):
         self.vars = {}
         
-        for instr in ast:
+        ip = 0 # instruction pointer
+        
+        while True:
+            if ip >= len(ast):
+                break
+            
+            instr = ast[ip]
+            
             if isinstance(instr, VarDefinition):
                 identifier = instr.identifier.identifier
                 if identifier in self.vars.keys():
@@ -31,8 +39,41 @@ class Interpreter():
             elif isinstance(instr, Print):
                 value = self.expression(instr.expression)
                 print value
+            elif isinstance(instr, WhileStart):
+                if not self.comparision(instr.expression1, instr.expression2, instr.compare_op):
+                    i = ip + 1
+                    stack = 0
+                    while True:
+                        if isinstance(ast[i], WhileEnd):
+                            if stack == 0:
+                                break
+                            else:
+                                stack -= 1
+                        elif isinstance(ast[i], WhileStart):
+                            stack += 1
+                        i += 1
+                        if i == len(ast):
+                            raise InterpreterError('while start is last instruction in program')
+                    ip = i
+            elif isinstance(instr, WhileEnd):
+                i = ip - 1
+                stack = 0
+                while True:
+                    if isinstance(ast[i], WhileStart):
+                        if stack == 0:
+                            break
+                        else:
+                            stack -= 1
+                    elif isinstance(ast[i], WhileEnd):
+                        stack += 1
+                    i -= 1
+                    if i < 0:
+                        raise InterpreterError('endwhile without while')
+                ip = i - 1
             
+            ip += 1
             
+    
     def expression(self, expression):
         value = 0
         for term in expression.terms:
@@ -59,9 +100,16 @@ class Interpreter():
             else:
                 value = val
         return value
-            
-            
-            
-            
-
+    
+    def comparision(self, expression1, expression2, operator):
+        exp1 = self.expression(expression1)
+        exp2 = self.expression(expression2)
+        
+        if operator == '>':
+            return exp1 > exp2
+        elif operator == '<':
+            return exp1 < exp2
+        else:
+            raise InterpreterError('unsupported comparision operator')
+        
         
--- compiler/main.py
+++ compiler/main.py
@@ -1,29 +1,34 @@
+#import sys
+
 from compiler.tokenizer import Tokenizer
 from compiler.parser import Parser
 from compiler.interpreter import Interpreter
-from compiler.symbols import TOKENS
+#from compiler.symbols import TOKENS
 
-f = open('../examples/vars', 'r')
+f = open('../examples/fib', 'r')
 code = f.read()
 f.close()
 
 tokenizer = Tokenizer()
 tokens = tokenizer.tokenize(code)
 
-
+"""
 for line in tokens:
     for token in line:
-        print TOKENS[token[0]]
+        sys.stdout.write(TOKENS[token[0]])
+        if len(token) == 2:
+            sys.stdout.write('(%s)' % token[1])
+        sys.stdout.write(' ')
     print
+"""
 
 parser = Parser()
 ast = parser.parse(tokens)
 
-
+"""
 for a in ast:
     print a
-
-exit()
+"""
 
 interpreter = Interpreter()
 interpreter.interpret(ast)
